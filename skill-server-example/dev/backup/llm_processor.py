a7e2b42ef7a1d260269baf4dde0045811252b7d9
Yoonha Jang <72743236+yooona1214@users.noreply.github.com>
2025-07-01 17:25:09 +0900

    Update llm_processor.py

--- skill-server-example/dev/backup/llm_processor.py
+++ skill-server-example/dev/backup/llm_processor.py
@@ -36,7 +36,6 @@ from langchain_experimental.agents import create_pandas_dataframe_agent
 
 import pika
 
-OPENAI_API_KEY = "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"
 
 # RabbitMQ 연동 위한 채널 큐 설정
 HOST_NAME = "localhost"
